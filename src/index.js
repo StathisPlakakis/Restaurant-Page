3c1862e6b19db8a8947d822f55d2f80f2a1e1c65
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 00:14:19 +0300

    Add an image to my assets

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('it works');
+import Image from './images/background-image.jpg';
+
+function component() {
+    const element = document.createElement('div');
+
+    const myImage = new Image();
+    myImage.src = Image;
+
+    element.appendChild(myImage);
+
+    return element;
+} 
+
+document.body.appendChild(component());
--- webpack.common.js
+++ webpack.common.js
@@ -15,19 +15,19 @@ module.exports = {
         }),
     ],
     module: {
-        rules: [
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: 'asset/resource',
-          },
-         {
-           test: /\.(woff|woff2|eot|ttf|otf)$/i,
-           type: 'asset/resource',
-         },
-        ],
-      },
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+       {
+         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+         type: 'asset/resource',
+       },
+      ],
+    },
 }

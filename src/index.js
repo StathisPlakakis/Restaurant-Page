074c8f8afd3da77fdb694aa3122d33c6e8bd4dcf
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 00:44:36 +0300

    Install webpack-merge

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import Image from './images/background-image.jpg';
+import './style.css';
+// import Image from './images/background-image.jpg';
 
 function component() {
     const element = document.createElement('div');
+    element.classList.add('hello');
 
-    const myImage = new Image();
-    myImage.src = Image;
+    const h1 = document.createElement('h1');
+    h1.textContent = 'HIIIIIII';
+    element.appendChild(h1);
 
-    element.appendChild(myImage);
+    // const myImage = new Image();
+    // myImage.src = Image;
+
+    // element.appendChild(myImage);
 
     return element;
 } 

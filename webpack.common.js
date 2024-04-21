e039d5daf5fcf56c97f80fc40082cfe2ca6cf3dd
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-21 21:45:28 +0300

    Delete config file
    I merged development and production code

--- webpack.common.js
+++ webpack.common.js
@@ -4,9 +4,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-    },
+        clean: true,
+      },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',

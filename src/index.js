eca0661de7dbf1e003a45df965debfdd1291ba8c
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 01:39:49 +0300

    Merge successfully prod & dev code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-// import Image from './images/background-image.jpg';
+import Image from './images/background-image.jpg';
 
 function component() {
     const element = document.createElement('div');
@@ -9,10 +9,10 @@ function component() {
     h1.textContent = 'HIIIIIII';
     element.appendChild(h1);
 
-    // const myImage = new Image();
-    // myImage.src = Image;
+    const myImage = new Image();
+    myImage.src = Image;
 
-    // element.appendChild(myImage);
+    element.appendChild(myImage);
 
     return element;
 } 

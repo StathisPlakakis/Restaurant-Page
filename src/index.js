2473118d7c44954e13887718e6a0114903e21cfa
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 10:33:19 +0300

    Add style.css and load image

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import './style.css';
-import backgroundImage from './images/background-image.jpg';
 
 function component() {
     const element = document.createElement('div');
-    element.classList.add('hello');
-
-    const h1 = document.createElement('h1');
-    h1.textContent = 'HI';
-    element.appendChild(h1);
-
-    const myImage = new Image();
-    myImage.src = backgroundImage;
-    element.appendChild(myImage);
-
+    element.setAttribute('id', 'content');
+    
     return element;
 } 
 

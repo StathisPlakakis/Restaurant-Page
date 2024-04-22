b269934976c30a5ae05f63f3dd2d435a7dd5b424
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 05:49:22 +0300

    Load the first image of the page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import './style.css';
-import Image from './images/background-image.jpg';
+import backgroundImage from './images/background-image.jpg';
 
 function component() {
     const element = document.createElement('div');
     element.classList.add('hello');
 
     const h1 = document.createElement('h1');
-    h1.textContent = 'HIIIIIII';
+    h1.textContent = 'HI';
     element.appendChild(h1);
 
     const myImage = new Image();
-    myImage.src = Image;
-
+    myImage.src = backgroundImage;
     element.appendChild(myImage);
 
     return element;

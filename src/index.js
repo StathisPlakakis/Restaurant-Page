aa31b820985d8edfef80b1eed4163e16db65ea9b
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-04-22 19:16:27 +0300

    Add a black filter for better contrast

--- src/index.js
+++ src/index.js
@@ -10,24 +10,30 @@ class display {
 
     content () {
         const header = document.querySelector('.header');
-
-
         const domain = document.querySelector('header');
         domain.textContent = "Soft Rest";
-
         const navBar = document.querySelector('nav');
+        const content = document.querySelector('#content');
 
         const home = document.createElement('li');
+        home.classList.add('home');
         home.textContent = "Home";
         navBar.appendChild(home);
 
         const menu = document.createElement('li');
+        menu.classList.add('menu');
         menu.textContent = "Menu";
         navBar.appendChild(menu);
 
         const contact = document.createElement('li');
+        contact.classList.add('contact');
         contact.textContent = "Contact";
         navBar.appendChild(contact);
+
+        const blackFilter = document.createElement('div');
+        blackFilter.classList.add('filter');
+        content.appendChild(blackFilter);
+
     }
 
     addContent () {
